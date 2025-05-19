7d30b65b28357679d067f6791d1d45a329044292
FeltyDima <103310112+FeltyDima@users.noreply.github.com>
2025-05-19 12:23:37 +0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>IT Company</title>
-    <link rel="stylesheet" href="css/style.css?version=51"/>
+    <link rel="stylesheet" href="css/style.css?version=52"/>
 </head>
 
 <body>
